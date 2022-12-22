81536e0f988bb4e2b4104d4ce85155261d6cafdd
Mk Asik <mkasik123@gmail.com>
2022-12-22 10:12:24 +0600

    finish polishing

--- src/Pages/Details/Details/Details.js
+++ src/Pages/Details/Details/Details.js
@@ -10,37 +10,37 @@ import Pdf from "react-to-pdf";
 
 const ref = React.createRef();
 const Details = () => {
-    const detail= useLoaderData();
+    const detail = useLoaderData();
     console.log(detail);
     return (
         <div className='detail'>
-               
-                <h2 className='mb-4'>Welcome to {detail.name} Course <Pdf targetRef={ref} filename="Course Details.pdf">
-        {({ toPdf }) => <Button onClick={toPdf} variant="light">Download</Button>}
-      </Pdf>  </h2>
-                
-                <Container >
-                    <Row>
-                        <Col><img className='photo' src={detail.img} alt="" /></Col>
-                        <Col>
-                        <Card  ref={ref} style={{ }}>
+
+            <h2 className='mb-4'>Welcome To {detail.name} Course <Pdf targetRef={ref} filename="Course Details.pdf">
+                {({ toPdf }) => <Button onClick={toPdf} variant="light">Download</Button>}
+            </Pdf>  </h2>
+
+            <Container >
+                <Row>
+                    <Col><img className='photo' src={detail.img} alt="" /></Col>
+                    <Col>
+                        <Card ref={ref} style={{}}>
                             <Card.Body>
                                 <Card.Title>{detail.name}</Card.Title>
-                                
+
                                 <Card.Text>
-                                {detail.details}
+                                    {detail.details}
                                 </Card.Text>
                                 <Card.Subtitle className="mb-2 text-muted">Course Fee: {detail.fee}</Card.Subtitle>
                                 <Card.Subtitle className="mb-2 text-muted">Course Duration: {detail.duration}</Card.Subtitle>
                                 <Card.Subtitle className="mb-2 text-muted">Total Modules: {detail.modules}</Card.Subtitle>
                                 <Link to={`/checkout/${detail.id}`}><Button variant="warning">Get premium access</Button></Link>
                             </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
-                    
-                    </Container>
-                   
+                        </Card>
+                    </Col>
+                </Row>
+
+            </Container>
+
         </div>
     );
 };
